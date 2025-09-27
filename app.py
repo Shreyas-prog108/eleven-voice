dfaa7c02a2b6514cac467562f0bcce973ef7ead4
Shreyas Pandey <shreyaspandey.ethical@gmail.com>
2025-09-27 18:20:43 +0530

    similarity to 90

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if st.button("Generate Voice"):
                 "text": text_input,
                 "voice_settings": {
                     "stability": 0.7,
-                    "similarity_boost": 0.35
+                    "similarity_boost": 0.90
                 }
             }
             response = requests.post(API_URL, headers=headers, json=data)
